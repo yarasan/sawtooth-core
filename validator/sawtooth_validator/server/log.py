c220200fd890600b5405bf9b95b038b0dc858a98
yarasan <yarasan@users.noreply.github.com>
2018-04-07 17:34:27 +0300

    Insert a blank line

--- validator/sawtooth_validator/server/log.py
+++ validator/sawtooth_validator/server/log.py
@@ -22,6 +22,7 @@ from colorlog import ColoredFormatter
 from sawtooth_validator.exceptions import LocalConfigurationError
 
 
+
 class LogWriter(object):
     def __init__(self, logger, level):
         self.logger = logger
